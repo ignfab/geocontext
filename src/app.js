02aecdfd0fb6f2bfe8235a82210c2c97d7e2e0be
MBorne <mickael.borne@ign.fr>
2025-03-07 10:22:03 +0100

    chore(logs): skip /health

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ const morganMiddleware = morgan(
             // Configure Morgan to use our custom logger with the http severity
             write: (message) => logger.info(message.trim()),
         },
-    }
+        skip: (req, res) => req.url === '/health'
+    },
 );
 app.use(morganMiddleware);
 
