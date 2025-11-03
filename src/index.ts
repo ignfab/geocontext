fca130bfc719cee60c2740a39667a7ea6b900490
MBorne <mickael.borne@ign.fr>
2025-11-03 09:58:07 +0100

    fix: define MCP name and version to avoid package.json lookup (refs #7)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import { MCPServer } from "mcp-framework";
 
-import { dirname } from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import { readFileSync } from "fs";
 
 // Get the directory of the current module (dist directory)
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Get version from package.json
+const pkgMetadata = JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf-8'));
+const VERSION = pkgMetadata.version;
+
 /**
  * Available transports
  */
-const TRANSPORTS : Record<string, any> = {
-  'stdio' : {
+const TRANSPORTS: Record<string, any> = {
+  'stdio': {
     type: "stdio"
   },
-  'http' : {
+  'http': {
     type: "http-stream",
     options: {
       port: 3000,
@@ -28,12 +33,14 @@ const TRANSPORTS : Record<string, any> = {
 async function main() {
   // get transport type from environment variable
   const TRANSPORT_TYPE = process.env.TRANSPORT_TYPE || "stdio";
-  if ( TRANSPORT_TYPE !== "stdio" && TRANSPORT_TYPE !== "http" ){
+  if (TRANSPORT_TYPE !== "stdio" && TRANSPORT_TYPE !== "http") {
     throw new Error(`Invalid transport type: ${TRANSPORT_TYPE}`);
   }
 
   // start MCP server
   const mcpServer = new MCPServer({
+    name: 'geocontext',
+    version: VERSION,
     basePath: __dirname,
     transport: TRANSPORTS[TRANSPORT_TYPE],
   });
