ce7e20c34ea45b099e0acfa738c5d21da6e9728a
esgn <5435148+esgn@users.noreply.github.com>
2026-04-23 16:13:56 +0200

    chore(toolchain): reduce npm deprecation noise and modernize build/test config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,20 +1,21 @@
-import { createJsWithTsEsmPreset, type JestConfigWithTsJest } from 'ts-jest'
+import { createJsWithTsEsmPreset } from 'ts-jest';
 
 const MILLISECONDS = 1000;
 
 const presetConfig = createJsWithTsEsmPreset({
   tsconfig: 'tsconfig.test.json',
-})
+});
 
-const jestConfig: JestConfigWithTsJest = {
+/** @type {import("jest").Config} */
+const jestConfig = {
   ...presetConfig,
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}'
+    'src/**/*.{ts,tsx,js,jsx}',
   ],
-  testTimeout: 60 * MILLISECONDS
-}
+  testTimeout: 60 * MILLISECONDS,
+};
 
 export default jestConfig;
