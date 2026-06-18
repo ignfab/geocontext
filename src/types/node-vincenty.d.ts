ba086cc9ea68ce4d06c2f4758ac4705cc6b0b202
Lionel Zoubritzky <lionel.zoubritzky@ign.fr>
2026-06-18 09:23:41 +0200

    fix: Improve distance computation

---
+++ src/types/node-vincenty.d.ts
@@ -0,0 +1,15 @@
+declare module "node-vincenty" {
+  export interface VincentyDistanceResult {
+    distance: number;
+    initialBearing: number;
+    finalBearing: number;
+  }
+
+  export function distVincenty(
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number,
+    callback?: (distance: number, initialBearing?: number, finalBearing?: number) => void,
+  ): VincentyDistanceResult | number | null;
+}
